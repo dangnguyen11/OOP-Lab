b61cef5f8f378ed593f846046219cb1a332b949a
Dang Nguyen <Dang.NH226019@sis.hust.edu.vn>
2024-03-15 20:45:26 +0700

    2.2.6.1

---
+++ Lab1/SolveEquation.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class SolveEquation{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a: ");
+        double a = input.nextDouble();
+
+        System.out.print("Enter b: ");
+        double b = input.nextDouble();
+//ab
+        if(a==0 && b==0)
+        {
+            System.out.print("Infinite solution");
+        }
+        else if(a==0 && b!=0)
+        {
+            System.out.print("No Solution");
+        }
+        else {
+            double result = -b/a;
+            System.out.print(result);
+        }
+
+
+        input.close();
+    }
+}
