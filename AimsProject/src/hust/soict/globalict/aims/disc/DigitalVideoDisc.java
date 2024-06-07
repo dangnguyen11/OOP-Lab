f693ed087da7a17a01237d9d31cab47a75c28967
Dang Nguyen <Dang.NH226019@sis.hust.edu.vn>
2024-06-07 21:07:14 +0700

    Lab3

---
+++ AimsProject/src/hust/soict/globalict/aims/disc/DigitalVideoDisc.java
@@ -0,0 +1,40 @@
+package hust.soict.globalict.aims.disc;
+
+public class DigitalVideoDisc extends Disc implements Playable {
+    public DigitalVideoDisc(String title) {
+        super(title);
+    }
+
+    public DigitalVideoDisc(String title, String category, float cost) {
+        super(title, category, cost);
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, float cost) {
+        super(title, category, director, cost);
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        super(title, category, director, length, cost);
+    }
+
+    public String toString() {
+        StringBuffer str = new StringBuffer();
+        str.append("DVD - ")
+                .append(getTitle())
+                .append(" - ")
+                .append(getCategory())
+                .append(" - ")
+                .append(getDirector() != null ? getDirector() : "Unknown")
+                .append(" - ")
+                .append(getLength() != 0 ? getLength() + " min" : "Unknown")
+                .append(" : ")
+                .append(getCost())
+                .append("$");
+        return str.toString();
+    }
+
+    public void play() {
+        System.out.println("Playing DVD: " + getTitle());
+        System.out.println("DVD length: " + getLength());
+    }
+}
